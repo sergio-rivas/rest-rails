cce5367fff2641a9059744a7fad1e12bbb2694ed
Sergio Rivas <sergiorivas@163.com>
2020-05-24 20:45:06 -0500

    update version, add PUT to update acceptability

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ RestRails::Engine.routes.draw do
 
   get    '/:table_name/:id'        => 'data#show',         as: 'data_show'
   patch  '/:table_name/:id'        => 'data#update'
+  put  '/:table_name/:id'          => 'data#update'
   delete '/:table_name/:id'        => 'data#destroy',      as: 'data_destroy'
 
   post   '/:table_name/:id/attach/:attachment_name' => 'data#attach'
--- lib/rest_rails/version.rb
+++ lib/rest_rails/version.rb
@@ -1,3 +1,3 @@
 module RestRails
-  VERSION = '1.1.5'
+  VERSION = '1.1.6'
 end
